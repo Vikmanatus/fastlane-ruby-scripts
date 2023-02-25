b9c4a1424baadc05eb15b7ea28e3bd9d66c8b08c
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-02-25 20:27:27 +0100

    successfully detecting instance of some fastlane actions

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
 require 'fastlane'
 
-actions_list = Fastlane::Actions.get_all_official_actions
+Fastlane::Actions.load_default_actions
+list = Fastlane::Actions.get_all_official_actions
+list.each do |element|
+  action_name = String(element).split('_').map(&:capitalize).join('')
+  instance = Fastlane::Actions.action_class_ref(action_name)
+  puts "Trying to find instance to call description method #{element.to_s}"
+end
+
+
+puts "Trying to fecth action list"
