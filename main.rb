734df6279c6a6d3d613cacff0107d0893e3bb9e3
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-02-25 20:41:05 +0100

    successfully getting description when method available

--- main.rb
+++ main.rb
@@ -6,6 +6,10 @@ list = Fastlane::Actions.get_all_official_actions
 list.each do |element|
   action_name = String(element).split('_').map(&:capitalize).join('')
   instance = Fastlane::Actions.action_class_ref(action_name)
+  if instance
+    description_method = instance.method(:description)
+    description_content = description_method.call
+  end
   puts "Trying to find instance to call description method #{element.to_s}"
 end
 
