a9918df4baec9d2c28b0bf66c996d12d3fd7fba8
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-04-24 22:35:00 +0200

    added merged list

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 require 'fastlane'
 require 'json'
 
-Fastlane::Actions.load_default_actions
+
 def config_item_to_hash(config_item)
   {
     key: config_item.key,
@@ -13,6 +13,16 @@ def config_item_to_hash(config_item)
     is_string: config_item.is_string,
     data_type: config_item.data_type
   }
+end
+
+action_file_paths = Fastlane::Actions.load_default_actions
+file_paths_list = action_file_paths.map do |file_path|
+  action_name = File.basename(file_path, '.rb')
+  { 'action_name' => action_name, 'file_path' => file_path }
+end
+
+File.open('./file_path.json', 'w') do |f|
+  f.write(JSON.pretty_generate(file_paths_list))
 end
 
 list = Fastlane::Actions.get_all_official_actions
@@ -33,8 +43,8 @@ list.each do |element|
           if config_item.is_a?(FastlaneCore::ConfigItem)
             config_item_to_hash(config_item)
           else
-            # puts "ACTION #{action_name} HAS INVALID CONFIG ITEM: #{config_item}"
             # TODO: fix the invalid config elements
+            puts "ACTION #{action_name} HAS INVALID CONFIG ITEM: #{config_item}"
           end
         end
         completion_list.append({ 'action_name' => action_name, 'args' => hash_list })
@@ -58,3 +68,29 @@ puts "INVALID INSTANCES: #{invalid_action_number}"
 File.open('./temp.json', 'w') do |f|
   f.write(JSON.pretty_generate(completion_list))
 end
+# Create a hash to store merged elements
+merged_hash = {}
+
+# Add all elements from list_1 to the merged_hash
+file_paths_list.each do |element|
+  action_name = element["action_name"]
+  merged_hash[action_name] = element
+end
+
+# Merge elements from list_2 into the merged_hash
+completion_list.each do |element|
+  action_name = element["action_name"]
+  if merged_hash.key?(action_name)
+    merged_hash[action_name] = merged_hash[action_name].merge(element)
+  else
+    merged_hash[action_name] = element
+  end
+end
+
+# Convert the merged_hash back into an array
+merged_list = merged_hash.values
+File.open('./merged.json', 'w') do |f|
+  f.write(JSON.pretty_generate(merged_list))
+end
+
+# Merging the lists
