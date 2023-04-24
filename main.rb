c7ba21689f9e215c4898093f52ef67b2aaac045b
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-04-24 18:27:22 +0200

    cleaning

--- main.rb
+++ main.rb
@@ -3,7 +3,6 @@ require 'fastlane'
 require 'json'
 
 Fastlane::Actions.load_default_actions
-
 def config_item_to_hash(config_item)
   {
     key: config_item.key,
@@ -17,20 +16,39 @@ def config_item_to_hash(config_item)
 end
 
 list = Fastlane::Actions.get_all_official_actions
+valid_action_number = 0
+invalid_action_number = 0
 list.each do |element|
+  action_name = element.to_s
   instance = Fastlane::Actions.action_class_ref(element)
-  next unless instance
+  if instance
+    puts "INSTANCE #{action_name}"
+    valid_action_number += 1
+    begin
 
-  begin
-    options_check = instance.method('available_options')
-    if !options_check.call.nil? && !options_check.call.empty?
-      hash_list = options_check.call.map do |config_item|
-        config_item.is_a?(FastlaneCore::ConfigItem) ? config_item_to_hash(config_item) : nil
+      options_check = instance.method('available_options')
+      if !options_check.call.nil? && !options_check.call.empty?
+        hash_list = options_check.call.map do |config_item|
+          if config_item.is_a?(FastlaneCore::ConfigItem)
+            config_item_to_hash(config_item)
+          else
+            # puts "ACTION #{action_name} HAS INVALID CONFIG ITEM: #{config_item}"
+          end
+        end
+        json_list = JSON.pretty_generate(hash_list)
+        # puts "Fastlane_Action: #{json_list}"
       end
-      json_list = JSON.pretty_generate(hash_list)
-      puts "#{instance.to_s}: #{json_list}"
+    rescue NameError => e
+      invalid_action_number += 1
+      puts "Skipping #{instance.to_s} due to an issue with uninitialized constants: #{e.message}"
     end
-  rescue NameError => e
-    puts "Skipping #{instance.to_s} due to an issue with uninitialized constants: #{e.message}"
+  else
+    invalid_action_number += 1
+    puts "NOT AN INSTANCE #{action_name}"
   end
 end
+
+puts "TOTAL ACTIONS: #{list.length}"
+puts "VALID INSTANCES: #{valid_action_number}"
+puts "INVALID INSTANCES: #{invalid_action_number}"
+
